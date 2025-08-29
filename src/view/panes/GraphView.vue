bc1b0487076c3c40b53f0cfd9995d579a19e07ec
Justin Lacy <justin.m.lacy@gmail.com>
2025-08-28 22:55:23 -0700

    scroll wheel zoom graph

--- src/view/panes/GraphView.vue
+++ src/view/panes/GraphView.vue
@@ -25,6 +25,37 @@ const xstops = computed(() => {
 	return axis.value.getTicks(range.value);
 });
 
+function onWheel(e: WheelEvent) {
+
+	e.preventDefault();
+	zoomChart(e.deltaY / 1000);
+}
+
+/**
+ * 
+ * @param pct - percent of range to zoom.
+ * pct>0 zooms out. pct < 0 zooms in.
+ */
+function zoomChart(pct: number) {
+
+	const [min, max] = range.value;
+
+	if ((pct < 0 && (max - min) < 0.00000001)
+		|| (pct > 0 && (max - min) > Number.MAX_SAFE_INTEGER / 1000)) {
+		return;
+	}
+
+	// prevent min/max overlap.
+	if (pct <= -0.4) pct = -0.4;
+
+	const amt = pct * (max - min);
+
+	range.value = [min - amt, max + amt];
+
+	buildChart();
+
+}
+
 function buildChart() {
 
 	if (!graphEl.value) {
@@ -73,7 +104,7 @@ watch(() => props.curves, (list) => {
 <template>
 
 	<div class="w-full h-full flex flex-col items-center">
-		<div class="grow" ref="graphEl"></div>
+		<div class="grow" ref="graphEl" @wheel="onWheel"></div>
 		<Range title="x-range:" v-model="range" />
 	</div>
 
